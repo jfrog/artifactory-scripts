e78e454224b15ebb77805661e79a7dd0ea5dd535
Jean Deruelle <jean.deruelle@telestax.com>
2014-07-22 23:07:09 +0200

    More info in comments

--- Artifactory-Cleanup.sh
+++ Artifactory-Cleanup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #replace <username> with your artifactory username
 #replace <password> with your artifactory password
-#replace <repo> with your artifactory domain name
+#replace <domain> with your artifactory domain name
+# pass to the script the $START_TIME for minimum creation time in milliseconds of artifacts  (example 1325376000000 for January 1st 2012)
+# pass to the script the $END_TIME for maximum creation time in milliseconds of artifacts  (example 1388534400000 for January 1st 2014)
+# pass to the script the $REPO for the repository where artifacts are located (example: releases)
 
 # find all artifacts created in 2013, time are expressed in milliseconds
 # grep look for uri attribute in JSON results returned then awk take the corresponding value of the uri attribute. The first 2 sed commands remove the last character and the last sed command remove the first character, ie stripping off the comma and double quotes from the URL in awk  
-RESULTS=`curl -s -X GET -u <username>:<password> "https://<repo>.artifactoryonline.com/<repo>/api/search/creation?from=$START_TIME&to=$END_TIME&repos=$REPO" | grep uri | awk '{print $3}' | sed s'/.$//' | sed s'/.$//' | sed -r 's/^.{1}//'`
+RESULTS=`curl -s -X GET -u <username>:<password> "https://<domain>.artifactoryonline.com/<domain>/api/search/creation?from=$START_TIME&to=$END_TIME&repos=$REPO" | grep uri | awk '{print $3}' | sed s'/.$//' | sed s'/.$//' | sed -r 's/^.{1}//'`
 for RESULT in $RESULTS ; do
     echo "fetching path from $RESULT"
     # from the URL we fetch the download uri to remove

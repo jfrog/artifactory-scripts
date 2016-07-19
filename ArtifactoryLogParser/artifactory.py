df6f32f40669a8bb5f83063f3c0e98302b69c9c0
Omar Faizyar <omar@Omars-MacBook-Pro.local>
2016-07-19 09:57:03 -0700

    Fixed artifactory.py
    Fixed artifactory.py

--- ArtifactoryLogParser/artifactory.py
+++ ArtifactoryLogParser/artifactory.py
@@ -9,42 +9,27 @@ if len(sys.argv) !=2:
     sys.exit(0)
 
 
+
 readfile = sys.argv[1]
 i = 0
 with open(readfile) as f:
     for line in f:
-    	if "WARN" in line:
-    		try:
-    			regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-    			match = re.search(regex,line.encode('ascii'),flags = 0)
-    			print Fore.RED + "WARNING:" + (match.group(6))
-           
-    		except Exception:
-    			pass
-
-with open(readfile) as f:
-    for line in f:
-    	if "ERROR" in line:
-    		try:
-    			regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-    			match = re.search(regex,line.encode('ascii'),flags = 0)
-    			print Fore.YELLOW + "ERROR:" + (match.group(6))
-            
-    		except Exception:
-    			pass
-
-with open(readfile) as f:
-    for line in f:
-        if "INFO" in line:
-            try:
+        try:
+           if "WARN" in line:
+                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                match = re.search(regex,line.encode('ascii'),flags = 0)
+                print Fore.RED + "WARNING:" + (match.group(6))
+           elif "ERROR" in line:
+                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                match = re.search(regex,line.encode('ascii'),flags = 0)
+                print Fore.YELLOW + "ERROR:" + (match.group(6))
+           elif "INFO" in line:
                 regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
                 match = re.search(regex,line.encode('ascii'),flags = 0)
                 print Fore.GREEN + "INFO:" + (match.group(6))
-            
-            except Exception:
-                pass
-    
+        except Exception:
+            pass
 print (Style.RESET_ALL)
 exit()
-    	
+        
  

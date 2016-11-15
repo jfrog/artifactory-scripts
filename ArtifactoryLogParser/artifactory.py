76a7c7f8d33875a2368ced9a3dededa98f9897b9
Kuzcoh <racc00n@riseup.net>
2016-11-14 22:23:08 -0800

    added dictionary and sorting

--- ArtifactoryLogParser/artifactory.py
+++ ArtifactoryLogParser/artifactory.py
@@ -3,33 +3,47 @@ import re
 import sys
 import colorama
 from colorama import Fore, Back, Style
+import operator
 
 if len(sys.argv) !=2:
     print('Usage: python artifactory.py artifactory.log')
     sys.exit(0)
 
-
-
+errors = []
+warnings = []
 readfile = sys.argv[1]
-i = 0
-with open(readfile) as f:
-    for line in f:
-        try:
-           if "WARN" in line:
-                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.RED + "WARNING:" + (match.group(6))
-           elif "ERROR" in line:
-                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.YELLOW + "ERROR:" + (match.group(6))
-           elif "INFO" in line:
-                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.GREEN + "INFO:" + (match.group(6))
-        except Exception:
-            pass
-print (Style.RESET_ALL)
+
+def readlogs():
+    with open(readfile) as f:
+        for line in f:
+            try:
+               if "WARN" in line:
+                    regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                    match = re.search(regex,line.encode('ascii'),flags = 0)
+                    print Fore.RED  + (match.group(6))
+                    warnings.append(match.group(6)[:40])
+
+               elif "ERROR" in line:
+                    regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                    match = re.search(regex,line.encode('ascii'),flags = 0)
+                    print Fore.YELLOW  + (match.group(6))
+                    errors.append(match.group(6)[:40])
+               elif "INFO" in line:
+                    regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                    match = re.search(regex,line.encode('ascii'),flags = 0)
+                    print Fore.GREEN + (match.group(6))
+            except Exception:
+                pass
+    warningdic = dict((x,warnings.count(x)) for x in set(warnings))
+
+    for key, value in sorted(warningdic.iteritems(), key=lambda (k,v): (v,k)):
+        print Fore.RED + "%s : times --- %s" % (key, value)
+
+    errorsdic = dict((x,errors.count(x)) for x in set(errors))
+    for key, value in sorted(errorsdic.iteritems(), key=lambda (k,v): (v,k)):
+        print Fore.YELLOW + "%s : times --- %s" % (key, value)
+    print (Style.RESET_ALL)
+
+readlogs()
 exit()
-        
- 
+
--- ArtifactoryLogParser/request.py
+++ ArtifactoryLogParser/request.py
@@ -1,14 +1,20 @@
 import sys
 import re
+import colorama
+from colorama import Fore, Back, Style
 #match.group(1) = time(example: 20160621065454)
 #match.group(4) = IP Address
 #match.group(6) = HTTP Method(GET,POST,PUT)
 #match.group(7) = URL
 #match.group(9) = HTTP Response Code
 
+responsecodes = []
+
+
 if len(sys.argv) !=2:
     print('Usage: python request.py request.log')
     sys.exit(0)
+
 readfile = sys.argv[1]
 with open(readfile) as f:
     for line in f:

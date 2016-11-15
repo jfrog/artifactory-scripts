47f2f1334b8d71a37ca63105276db33279f76972
Omar Faiz <omarf@omarf-mac.inc.jfrog.local>
2016-11-15 09:26:22 -0800

    artifactory.py can parse debuggers now

--- ArtifactoryLogParser/artifactory.py
+++ ArtifactoryLogParser/artifactory.py
@@ -11,6 +11,7 @@ if len(sys.argv) !=2:
 
 errors = []
 warnings = []
+debug = []
 readfile = sys.argv[1]
 
 def readlogs():
@@ -32,16 +33,26 @@ def readlogs():
                     regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
                     match = re.search(regex,line.encode('ascii'),flags = 0)
                     print Fore.GREEN + (match.group(6))
+               elif "DEBUG" in line:
+                    regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+                    match = re.search(regex,line.encode('ascii'),flags = 0)
+                    print Fore.MAGENTA + line
+                    debug.append(match.group(6)[:40])               
             except Exception:
                 pass
     warningdic = dict((x,warnings.count(x)) for x in set(warnings))
-
     for key, value in sorted(warningdic.iteritems(), key=lambda (k,v): (v,k)):
         print Fore.RED + "%s : times --- %s" % (key, value)
 
     errorsdic = dict((x,errors.count(x)) for x in set(errors))
     for key, value in sorted(errorsdic.iteritems(), key=lambda (k,v): (v,k)):
         print Fore.YELLOW + "%s : times --- %s" % (key, value)
+
+    debugdic = dict((x,debug.count(x)) for x in set(debug))
+    for key,value in sorted(debugdic.iteritems(),key=lambda (k,v):(v,k)):
+        print Fore.MAGENTA + "%s : times --- %s" % (key,value)
+
+
     print (Style.RESET_ALL)
 
 readlogs()

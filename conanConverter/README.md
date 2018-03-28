21c7247a8e8992a474a220fb0bc5196ba4fde4e9
travisf <travisf@jfrog.com>
2018-03-28 13:22:20 -0700

    Added conanConverter

---
+++ conanConverter/README.md
@@ -0,0 +1,17 @@
+conanConverter.py
+=================
+
+This script will copy Conan packages in the layout
+`[name]/[version]/[user]/[channel]` to the layout
+`[user]/[name]/[version]/[channel]` in the same repository. Since this is a
+copy, the original package will still exist.
+
+To run:
+
+```
+./conanConverter.py <artifactory url> <username> <password> <repository>
+```
+
+Run this script only once per repository. If it is run a second time, due to the
+way directory copies are handled, a second copy of the package will be inserted
+inside of the original copy.
---
+++ conanConverter/conanConverter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2
+
+import re
+import sys
+import json
+import urllib2
+import base64
+
+class PostRequest(urllib2.Request):
+    def __init__(self, *args, **kwargs):
+        urllib2.Request.__init__(self, *args, **kwargs)
+
+    def get_method(self, *args, **kwargs):
+        return 'POST'
+
+def getaql(repo):
+    query = {}
+    query['repo'] = repo
+    query['type'] = 'folder'
+    query['depth'] = '4'
+    query['path'] = {'$match': '*/*.*.*/*'}
+    return 'items.find(' + json.dumps(query) + ').include("name","repo","path")'
+
+def reqaql(url, user, pasw, repo):
+    path = url
+    if path[-1] != '/': path += '/'
+    path += 'api/search/aql'
+    auth = 'Basic ' + base64.b64encode(user + ':' + pasw)
+    headers = {'Content-Type': 'text/plain', 'Authorization': auth}
+    req = PostRequest(path, getaql(repo), headers)
+    resp = urllib2.urlopen(req)
+    return json.load(resp)
+
+def reqcp(url, user, pasw, pathfrom, pathto):
+    path = url
+    if path[-1] != '/': path += '/'
+    path += 'api/copy' + pathfrom + '?to=' + pathto #+ '&dry=1'
+    auth = 'Basic ' + base64.b64encode(user + ':' + pasw)
+    headers = {'Content-Type': 'text/plain', 'Authorization': auth}
+    req = PostRequest(path, None, headers)
+    return json.load(urllib2.urlopen(req))
+
+def main():
+    if len(sys.argv) != 5:
+        print "Usage: ./conanConverter.py <artifactory url> <username> <password> <repository>"
+        return
+    url, user, pasw, repo = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    aqlresp = reqaql(url, user, pasw, repo)
+    for item in aqlresp['results']:
+        match = re.match(r'^([^/]+)/(\d+\.\d+\.\d+[^/]*)/([^/]+)$', item['path'])
+        if match == None: continue
+        newpath = match.group(3) + '/' + match.group(1) + '/' + match.group(2)
+        pathto = '/' + item['repo'] + '/' + newpath + '/' + item['name']
+        pathfrom = '/' + item['repo'] + '/' + item['path'] + '/' + item['name']
+        print "#### Copying " + pathfrom + " to " + pathto
+        print "     " + json.dumps(reqcp(url, user, pasw, pathfrom, pathto))
+
+if __name__ == '__main__': main()

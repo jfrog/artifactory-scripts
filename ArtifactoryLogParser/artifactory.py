fad095a82e0353261e128acb3194662016fc13e7
Angello Maggio <angellom@jfrog.com>
2018-12-13 04:03:35 -0800

    Improvements on artifactory.py. Easier to read and removed redundant code. Changes were made for backwards compatibility.

--- ArtifactoryLogParser/artifactory.py
+++ ArtifactoryLogParser/artifactory.py
@@ -1,48 +1,62 @@
 #!/usr/bin/python
 import re
 import sys
-import colorama
-from colorama import Fore, Back, Style
+
+try:
+    import colorama
+    from colorama import Fore, Back, Style
+    ce = True  # Color Enabled
+
+except ModuleNotFoundError:
+    print("Colorama is not installed. Coloring functions will be disabled.")
+    print("To enable coloring run 'pip install colorama'.")
+    print()
+    ce = False  # Color Enabled
+
+except Exception as e:
+    print("Unexpected exception during import of Colorama")
+    print(e)
 
 if len(sys.argv) !=2:
-    print('Usage: python artifactory.py artifactory.log')
+    print('Usage: python artifactory.py /path/to/artifactory.log')
     sys.exit(0)
 
 
+# Gather input values
 readfile = sys.argv[1]
-i = 0
+
 with open(readfile) as f:
-    for line in f:
+    for line in f.readlines():
         if "WARN" in line:
             try:
                 regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.RED + "WARNING:" + (match.group(6))
-           
-            except Exception:
-                pass
+                match = re.search(regex, line, flags=0)
+                if ce:
+                    print(Fore.RED, "WARNING:", (match.group(6)))
+                else:
+                    print("--")
+            except Exception as e:
+                print("[artifactory.py] Unexpected exception when parsing for warnings")
+                print(e)
 
-with open(readfile) as f:
-    for line in f:
         if "ERROR" in line:
             try:
-                regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.YELLOW + "ERROR:" + (match.group(6))
-            
-            except Exception:
-                pass
+                match = re.search(regex, line, flags=0)
+                print(Fore.YELLOW + "ERROR:" + (match.group(6)))
 
-with open(readfile) as f:
-    for line in f:
+            except Exception as e:
+                print("[artifactory.py] Unexpected exception when parsing for warnings")
+                print(e)
+
         if "INFO" in line:
             try:
                 regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
-                match = re.search(regex,line.encode('ascii'),flags = 0)
-                print Fore.GREEN + "INFO:" + (match.group(6))
+                match = re.search(regex, line, flags=0)
+                print(Fore.GREEN + "INFO:" + (match.group(6)))
             
-            except Exception:
-                pass
+            except Exception as e:
+                print("[artifactory.py] Unexpected exception when parsing for warnings")
+                print(e)
     
 print (Style.RESET_ALL)
 exit()

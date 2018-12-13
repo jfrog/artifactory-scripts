76fd8f6a45a807f6bfa3d5ee3669780b21eb2e08
Angello Maggio <angellom@jfrog.com>
2018-12-13 05:04:07 -0800

    Further improvements to make code more pythonic. Search filters are now available

--- ArtifactoryLogParser/artifactory.py
+++ ArtifactoryLogParser/artifactory.py
@@ -23,49 +23,71 @@ class Colors:
         print(self.ENDC)
 
 
-if len(sys.argv) !=2:
-    print('Usage: python artifactory.py /path/to/artifactory.log')
-    sys.exit(0)
+class Logger:
+
+    default_sf = ["WARN", "ERROR", "INFO"]
+
+    def __init__(self, readfile, regex):
+        self.colors = Colors()
+        self.data = {
+            'WARN':
+                {'results': [],
+                 'color': self.colors.WARNING,
+                 'message': 'WARNING:'},
+            'ERROR':
+                {'results': [],
+                 'color': Colors.FAIL,
+                 'message': 'ERROR:'},
+            'INFO':
+                {'results': [],
+                 'color': Colors.OKBLUE,
+                 'message': 'INFO:'
+                 }
+            }
+        self.readfile = readfile
+        self.regex = regex
+
+    def parse(self):
+        with open(self.readfile) as f:
+            for line in f.readlines():
+                match = re.search(self.regex, line, flags=0)
+                if match:
+                    e_type = [x for x in self.default_sf if x in line][0]
+                    entry = ' '.join([self.data[e_type]['color'], self.data[e_type]['message'], (match.group(6))])
+                    self.data[e_type]['results'].append(entry)
+
+    def print(self, sf):
+        for entry_type in self.data:
+            if entry_type in sf:
+                for result in self.data[entry_type]["results"]:
+                    print(result)
 
 
-# Gather input values
-readfile = sys.argv[1]
+def main():
 
-data = {'WARN':
-            {'results': [],
-             'color': Colors.WARNING,
-             'message': 'WARNING:'},
-        'ERROR':
-            {'results': [],
-             'color': Colors.FAIL,
-             'message': 'ERROR:'},
-        'INFO':
-            {'results': [],
-             'color': Colors.OKBLUE,
-             'message': 'INFO:'
-             }
-        }
+    # Gather input values
+    readfile = sys.argv[1]
+    regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+    logger = Logger(readfile, regex)
+
+    if len(sys.argv) < 2:
+        print('Usage: python artifactory.py /path/to/artifactory.log [filters]')
+        sys.exit(0)
+    elif len(sys.argv) == 2:
+        search_filter = logger.default_sf
+    else:
+        search_filter = sys.argv[2:]
+
+    logger.parse()
+    logger.print(sf=search_filter)
+    logger.colors.clear()
+    exit()
 
 
-regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+if __name__ == '__main__':
+    main()
 
-unexpected_error_log = "[artifactory.py] Unexpected exception when parsing for {}"
 
-with open(readfile) as f:
-    for line in f.readlines():
-        message = ""
-        match = re.search(regex, line, flags=0)
 
-        if match:
-            e_type = [x for x in ["ERROR", "WARN", "INFO"] if x in line][0]
-            entry = ' '.join([data[e_type]['color'], data[e_type]['message'], (match.group(6))])
-            data[e_type]['results'].append(entry)
 
-for entry_type in data:
-    for result in data[entry_type]["results"]:
-        print(result)
-
-Colors().clear()
-
-exit()
 

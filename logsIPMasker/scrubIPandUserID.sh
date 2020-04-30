fdd75376fe0350de2748eba55ce3a20fe33f7d8a
Ariel Seftel <ariels@jfrog.com>
2020-04-30 14:10:22 +0300

    Made some light changes

--- logsIPMasker/scrubIPandUserID.sh
+++ logsIPMasker/scrubIPandUserID.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-CURRENT_DIR=$pwd
+#!/bin/bash 
+CURRENT_DIR=$PWD
 echo $currentDir
 read -p "Please enter the \$BUNDLE_DIR. This will be a directory where the files to mask are located. `echo $'\n> '`" BUNDLE_DIR
 echo "Provided BUNDLE_DIR is: $BUNDLE_DIR, continue?"
@@ -9,7 +9,6 @@ select yin in "yes" "no"; do
         no  ) echo "Exiting"; exit;;
     esac
 done
-
 
 echo "Searching for archives to extract..."
 while [ "`find $BUNDLE_DIR -type f \( -name '*.zip' -o -name '*.tgz' -o -name '*.gz' -o -name '*.tar.gz' \) | wc -l`" -gt 0 ]
@@ -39,11 +38,19 @@ done
 echo "No archives remaining."
 
 echo "Looking for UserID and IP to replace in request, audit, and access files..."
-find $BUNDLE_DIR -type f \( -name 'access*.log' -o -name 'audit*.log' -o -name 'request*.log' -o -name 'artifactory*.log' \) | while read line; do
+find $BUNDLE_DIR -type f \( -name 'access*.log' -o -name 'audit*.log' -o -name 'request*.log' -o -name 'artifactory*.log' -o -name 'security.audit*.log' \) | while read line; do
 if [[ ${line##*/} == "access"*".log" ]]
     then
       echo "found access file, $line scrubbing sensitive data inside"
-      sed -i.jfrogbkp 's/client :.*/client : Scrubbed the IP and User/' $line
+      sed -i.jfrogbkp 's/\( *for client : *\)[^ ]*\(.*\)*$/for client : ScrubbedUserID\2/Ig' $line
+      sed -i.jfrogbkp -e 's/\( *user:  *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/Ig' -e ':a' -e 's/\( *user:  *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/' -e 'ta' $line
+      sed -i.jfrogbkp -e 's/\( *user *\)[^ ]*\(.*\)*$/User: ScrubbedUserID\2/Ig' -e ':a' -e 's/\( *user *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/' -e 'ta' $line
+      sed -i.jfrogbkp -e 's/\( *username  *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/Ig' -e ':a' -e 's/\( *username  *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/' -e 'ta' $line
+      sed -i.jfrogbkp -e 's/\( *user named  *\)[^ ]*\(.*\)*$/ User: ScrubbedUserID\2/Ig'  $line
+      sed -i.jfrogbkp -e 's/\( *User not found:  *\)[^ ]*\(.*\)*$/ Could not find User: ScrubbedUserID\2/Ig' $line
+      sed -i.jfrogbkp -e 's/\( *authentication failed for *\)[^ ]*\(.*\)*$/ authentication failed for User: ScrubbedUserID\2/Ig' $line
+      sed -i.jfrogbkp -e 's/\( *token for subject *\)[^ ]*\(.*\)*$/ token for User: ScrubbedUserID\2/Ig' $line
+      sed -i.jfrogbkp -e 's/\( *Auto-Creation of *\)[^ ]*\(.*\)*$/ Auto-Creation of User: ScrubbedUserID\2/Ig' $line
     elif [[ ${line##*/} == "audit"*".log" ]]
     then
       echo "found audit file, $line scrubbing sensitive data inside"
@@ -59,6 +66,10 @@ if [[ ${line##*/} == "access"*".log" ]]
       sed -i.jfrogbkp -e 's/\( *authentication failed for *\)[^ ]*\(.*\)*$/ authentication failed for User: ScrubbedUserID\2/Ig' $line
       sed -i.jfrogbkp -e 's/\( *token for subject *\)[^ ]*\(.*\)*$/ token for User: ScrubbedUserID\2/Ig' $line
       sed -i.jfrogbkp -e 's/\( *Auto-Creation of *\)[^ ]*\(.*\)*$/ Auto-Creation of User: ScrubbedUserID\2/Ig' $line
+    elif [[ ${line##*/} == "security.audit"*".log" ]]
+    then
+      echo "found security.audit file, $line scrubbing sensitive data inside"
+        echo "Scrubbing file due to security reasons" > $line
     elif [[ ${line##*/} == "request"*"log" ]]
     then
       echo "found request file, $line scrubbing sensitive data inside"
@@ -80,8 +91,6 @@ echo "Finished cleaning all tmp files"
 
 echo "Starting to zip all"
 cd $BUNDLE_DIR
-rm .\!*\!*.*
 zip -r JFrogBundleCleanIPs.$(date +%Y%m%d%H%M%S).zip .
 mv JFrogBundleCleanIPs* $CURRENT_DIR
 echo "DONE"
-

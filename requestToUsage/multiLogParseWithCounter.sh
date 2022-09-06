4b4b7b3b5f871d6b28d202c5b584f9cb5b680406
Anantha Kalusivalingam <107127129+ananthakalusivalingam@users.noreply.github.com>
2022-09-06 13:39:55 -0500

    Update multiLogParseWithCounter.sh
    Update for the $Delimiter

--- requestToUsage/multiLogParseWithCounter.sh
+++ requestToUsage/multiLogParseWithCounter.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 LOC=$1
+DELIMITER=$2
 ${LOC:-"."}
 if [ ! -d $LOC ]; then
-    echo "Usage is: ./multiLogParse.sh /path/to/logs/ <OPTIONAL_PREFIX>"
+    echo "Usage is: ./multiLogParseWithCounter.sh /path/to/logs/ <DELIMITER> <OPTIONAL_PREFIX>"
 else
     echo "Using directory $LOC as LOC"
 fi
 COUNTER=1
 cd $LOC
 for i in artifactory-request.*; do
-  .\requestToUsage.sh $LOC\\$i $COUNTER
+  .\requestToUsage.sh $LOC\\$i $DELIMITER $COUNTER
    COUNTER=$(( COUNTER + 1 ))
 done
